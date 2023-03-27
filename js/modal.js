79dc239c3972202f53c5df3dc880dbc942db88f2
chiri03 <lena.kiriac.97@gmail.com>
2023-03-27 13:11:20 +0300

    mobile-menu section finally work)))))

--- js/modal.js
+++ js/modal.js
@@ -12,17 +12,3 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
-
-const mobileNav = document.querySelector(".main_nav_list");
-const navToggle = document.querySelector(".mobile-nav-toggle");
-
-navToggle.addEventListener("click", () => {
-  const visibility = mobileNav.getAttribute("data-visible");
-
-  console.log(visibility);
-  if (visibility === "false") {
-    mobileNav.setAttribute("data-visible", true);
-  } else if (visibility === "true") {
-    mobileNav.setAttribute("data-visible", false);
-  }
-});
